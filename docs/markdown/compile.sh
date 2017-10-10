236c654c2621118d7b869feb7e78812bfd95a25d
Paul H <github@sphardy.com>
2017-10-10 13:06:51 +0300

    Update Markdown Compile Script

--- docs/markdown/compile.sh
+++ docs/markdown/compile.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-./lpp.pl ../tex/preamble.tex      > preamble.tex
-./lpp.pl ../tex/setup.tex         > setup.tex
-./lpp.pl ../tex/configuration.tex > configuration.tex
-./lpp.pl ../tex/introduction.tex  > introduction.tex
-./lpp.pl ../tex/history.tex       > history.tex
-./lpp.pl ../tex/interfaces.tex    > interfaces.tex
-./lpp.pl ../tex/references.tex    > references.tex
-./lpp.pl ../tex/resources.tex     > resources.tex
-./lpp.pl ../tex/setup.tex         > setup.tex
-./lpp.pl ../tex/specification.tex > specification.tex
+cd ../tex
 
-pandoc --default-image-extension=png -t markdown_github -B frontmatter -o ../../DATASHEET.md "AHB-Lite_PLIC_Markdown.tex"
+# Pre-process LaTeX Source
+for file in *.tex
+do
+ ../markdown/lpp.pl $file > ../markdown/$file
+done
 
-#echo '---' > /tmp/frontmatter
-#echo 'Title: PLIC Datasheet' >> /tmp/frontmatter
-#echo '---' >> /tmp/frontmatter
-#cat ../DATASHEET.md >> /tmp/frontmatter
-#cp /tmp/frontmatter ../DATASHEET.md
+cd ../markdown
+
+# Generate new Markdown
+pandoc --default-image-extension=png -t markdown_github -B frontmatter.md -o ../../DATASHEET.md "AHB-Lite_PLIC_Markdown.tex"
