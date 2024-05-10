fc6abe4ee04073539b77abfb6d53ee19b25bbc75
Richard Herveille <richard.herveille@roalogic.com>
2024-05-10 02:27:12 +0200

    Initial release

---
+++ rtl/filelist_ahb3lite.f
@@ -0,0 +1,8 @@
+verilog/core/plic_dynamic_registers.sv
+verilog/core/plic_priority_index.sv
+verilog/core/plic_cell.sv
+verilog/core/plic_target.sv
+verilog/core/plic_gateway.sv
+verilog/core/plic_core.sv
+verilog/ahb3lite/ahb3lite_plic_top.sv
+
---
+++ rtl/filelist_apb4.f
@@ -0,0 +1,8 @@
+verilog/core/plic_dynamic_registers.sv
+verilog/core/plic_priority_index.sv
+verilog/core/plic_cell.sv
+verilog/core/plic_target.sv
+verilog/core/plic_gateway.sv
+verilog/core/plic_core.sv
+verilog/apb4/apb4_plic_top.sv
+
